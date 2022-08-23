ac558a347c66714461c64ba3550f80f59ada1a48
arsen <chivalryytf@gmail.com>
2022-08-23 21:50:13 +0900

    some changes

--- _nuxt/static/1661258439/manager/events/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1661258439",layout:"adminLayout",error:a,state:a,serverRendered:false,routePath:"\u002Fmanager\u002Fevents",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
