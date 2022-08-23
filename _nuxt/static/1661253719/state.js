59ba863324480dc99208116edd56c17e0d7934cf
arsen <chivalryytf@gmail.com>
2022-08-23 20:22:12 +0900

    some changes

---
+++ _nuxt/static/1661253719/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1661253719",layout:"default",error:a,state:{authUser:a,loggedIn:false,account:{ACCOUNT:a,types:{}}},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,"\u002F"));
