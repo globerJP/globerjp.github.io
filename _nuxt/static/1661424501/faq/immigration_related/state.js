c9c2fe010407eadd9a1633a9d785dbcd9c014b04
arsen <chivalryytf@gmail.com>
2022-08-25 20:02:54 +0900

    some changes

--- _nuxt/static/1661424501/faq/immigration_related/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1661424501",layout:"default",error:a,state:{authUser:a,loggedIn:false,account:{ACCOUNT:a,types:{}}},serverRendered:true,routePath:"\u002Ffaq\u002Fimmigration_related",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
