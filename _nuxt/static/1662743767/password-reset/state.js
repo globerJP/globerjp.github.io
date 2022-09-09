00c7bd72d36076b8a11c3eb9c4e917d276412ca8
arsen <chivalryytf@gmail.com>
2022-09-10 02:23:02 +0900

    some changes

--- _nuxt/static/1662743767/password-reset/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1662743767",layout:"loginLayout",error:a,state:{authUser:a,loggedIn:false,account:{ACCOUNT:a,types:{}}},serverRendered:true,routePath:"\u002Fpassword-reset",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
