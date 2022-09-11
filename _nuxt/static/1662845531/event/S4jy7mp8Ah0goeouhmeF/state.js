182dbfd84333146d814ef80a11168fbb4845e1fc
arsen <chivalryytf@gmail.com>
2022-09-11 20:35:12 +0900

    some changes

--- _nuxt/static/1662845531/event/S4jy7mp8Ah0goeouhmeF/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1662845531",layout:"default",error:a,state:{authUser:a,loggedIn:false,account:{ACCOUNT:a,types:{}}},serverRendered:true,routePath:"\u002Fevent\u002FS4jy7mp8Ah0goeouhmeF",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
