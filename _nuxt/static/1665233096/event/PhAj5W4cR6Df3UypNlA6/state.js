72ae3a1aa8120bf17a05bbf8e27f1a82c1dbe2d6
arsen <arsenjp24@gmail.com>
2022-10-08 21:45:20 +0900

    some changes

---
+++ _nuxt/static/1665233096/event/PhAj5W4cR6Df3UypNlA6/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1665233096",layout:"default",error:a,state:{authUser:a,loggedIn:false,account:{ACCOUNT:a,types:{}}},serverRendered:true,routePath:"\u002Fevent\u002FPhAj5W4cR6Df3UypNlA6",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
