bf41a23cf0c324c0da79dbbfe9edf4cadfacaa37
arsen <arsenjp24@gmail.com>
2022-10-13 19:49:41 +0900

    some changes

--- _nuxt/173aa2f.js
+++
@@ -1,2 +0,0 @@
-/*! For license information please see LICENSES */
-(window.webpackJsonp=window.webpackJsonp||[]).push([[138],{1290:function(e,n,r){"use strict";r.r(n);var o=r(741);r.d(n,"default",(function(){return o.a}));o.a.registerVersion("firebase","9.9.3","app-compat")}}]);
