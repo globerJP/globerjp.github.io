bb6578f960339d198685c514811b5483be4f2b8d
arsen <arsenjp24@gmail.com>
2022-10-15 04:36:15 +0900

    some changes

---
+++ _nuxt/605bdd5.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[14],{983:function(n){n.exports=JSON.parse("{}")}}]);
---
+++ _nuxt/static/1665776093/chatNew/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/chatNew", {data:[{}],fetch:{},mutations:[]});
