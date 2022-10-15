2a6ac0a1bcbe190e899d52ca375fe82dc62c9246
arsen <arsenjp24@gmail.com>
2022-10-16 06:51:21 +0900

    some changes

--- _nuxt/static/1665866682/event/JHot1OdmTr3GMW8JI8Ir/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1665866682",layout:"default",error:a,state:{authUser:a,loggedIn:false,account:{ACCOUNT:a,types:{}}},serverRendered:true,routePath:"\u002Fevent\u002FJHot1OdmTr3GMW8JI8Ir",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
