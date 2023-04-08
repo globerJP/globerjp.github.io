753bf6f2c62cc1f3b728a13b9b1a9dbb0bed90bd
arsen <arsenjp24@gmail.com>
2023-04-08 10:49:23 +0900

    some changes

---
+++ _nuxt/static/1680918434/surveys/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1680918434",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002Fsurveys",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
